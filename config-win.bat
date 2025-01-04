dd370c60d7d59e71949a1c0e64322595d5492367
Ivan Enzhaev <8observer8@gmail.com>
2025-01-05 00:02:14 +0400

    Initial commit

---
+++ config-win.bat
@@ -0,0 +1,1 @@
+cmake -G "MinGW Makefiles" -S . -B dist/win -DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/win/lib/cmake/SDL3 -DSDL3_mixer_DIR=E:/libs/sdl3_mixer-3.0.0-prefix/win/lib/cmake/SDL3_mixer -DCMAKE_BUILD_TYPE=Debug
